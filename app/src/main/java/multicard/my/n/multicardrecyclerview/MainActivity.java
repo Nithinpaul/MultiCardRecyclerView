42a5f67e9a94f5c614afe771a7c37982558dc295
Nithinpaul <nithpl@gmail.com>
2016-10-07 11:45:52 +0530

    File Upload

--- app/src/main/java/multicard/my/n/multicardrecyclerview/MainActivity.java
+++ app/src/main/java/multicard/my/n/multicardrecyclerview/MainActivity.java
@@ -2,12 +2,48 @@ package multicard.my.n.multicardrecyclerview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
+import static multicard.my.n.multicardrecyclerview.myCustomAdapter.card1;
+import static multicard.my.n.multicardrecyclerview.myCustomAdapter.card2;
+import static multicard.my.n.multicardrecyclerview.myCustomAdapter.card3;
+
+/**
+ * Created by nithin
+ */
 public class MainActivity extends AppCompatActivity {
+
+    RecyclerView mRecyclerView;
+    private myCustomAdapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+    private String[] mDataset = {
+            "Card 1",
+            "Card 2",
+            "Card 3",
+            "Card 1"
+    };
+    int[] mDataSetTypes = {
+            card1,
+            card2,
+            card3,
+            card1
+    }; //view types
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+
+
+        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getApplicationContext());
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+
+        mAdapter = new myCustomAdapter(mDataset, mDataSetTypes);
+        mRecyclerView.setAdapter(mAdapter);
     }
 }
---
+++ app/src/main/java/multicard/my/n/multicardrecyclerview/myCustomAdapter.java
@@ -0,0 +1,111 @@
+package multicard.my.n.multicardrecyclerview;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+/**
+ * Created by nithin on 10/7/2016.
+ */
+
+public class myCustomAdapter extends RecyclerView.Adapter<myCustomAdapter.ViewHolder> {
+    private static final String TAG = "myCustomAdapter";
+
+    private String[] mDataSet;
+    private int[] mDataSetTypes;
+
+    public static final int card1 = 0;
+    public static final int card2 = 1;
+    public static final int card3 = 2;
+
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        public ViewHolder(View v) {
+            super(v);
+        }
+    }
+
+    public class WeatherViewHolder extends ViewHolder {
+        TextView temp;
+
+        public WeatherViewHolder(View v) {
+            super(v);
+            this.temp = (TextView) v.findViewById(R.id.temp);
+        }
+    }
+
+    public class ScoreViewHolder extends ViewHolder {
+        TextView score;
+
+        public ScoreViewHolder(View v) {
+            super(v);
+            this.score = (TextView) v.findViewById(R.id.score);
+        }
+    }
+
+    public class NewsViewHolder extends ViewHolder {
+        TextView headline;
+        Button read_more;
+
+        public NewsViewHolder(View v) {
+            super(v);
+            this.headline = (TextView) v.findViewById(R.id.headline);
+            this.read_more = (Button) v.findViewById(R.id.read_more);
+        }
+    }
+
+
+    public myCustomAdapter(String[] dataSet, int[] dataSetTypes) {
+        mDataSet = dataSet;
+        mDataSetTypes = dataSetTypes;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
+        View v;
+        if (viewType == card1) {
+            v = LayoutInflater.from(viewGroup.getContext())
+                    .inflate(R.layout.card1, viewGroup, false);
+
+            return new WeatherViewHolder(v);
+        } else if (viewType == card2) {
+            v = LayoutInflater.from(viewGroup.getContext())
+                    .inflate(R.layout.card2, viewGroup, false);
+            return new NewsViewHolder(v);
+        } else {
+            v = LayoutInflater.from(viewGroup.getContext())
+                    .inflate(R.layout.card3, viewGroup, false);
+            return new ScoreViewHolder(v);
+        }
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder viewHolder, final int position) {
+        if (viewHolder.getItemViewType() == card1) {
+            WeatherViewHolder holder = (WeatherViewHolder) viewHolder;
+            holder.temp.setText(mDataSet[position]);
+        }
+        else if (viewHolder.getItemViewType() == card2) {
+            NewsViewHolder holder = (NewsViewHolder) viewHolder;
+            holder.headline.setText(mDataSet[position]);
+        }
+        else {
+            ScoreViewHolder holder = (ScoreViewHolder) viewHolder;
+            holder.score.setText(mDataSet[position]);
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return mDataSet.length;
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        return mDataSetTypes[position];
+    }
+}
+
